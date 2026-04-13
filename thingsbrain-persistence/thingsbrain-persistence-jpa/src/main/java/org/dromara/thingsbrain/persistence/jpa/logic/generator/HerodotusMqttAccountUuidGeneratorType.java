af98e0d55e7602d9be4a10636eee4b5ad59969b4
码匠君 <pointer_v@qq.com>
2026-04-13 16:55:47 +0800

    [重构] 重构 hibernate 自定义扩展模块包名，去除多余的、与模块名称不匹配的包层次定义

--- thingsbrain-persistence/thingsbrain-persistence-jpa/src/main/java/org/dromara/thingsbrain/persistence/jpa/logic/generator/HerodotusMqttAccountUuidGeneratorType.java
+++ thingsbrain-persistence/thingsbrain-persistence-jpa/src/main/java/org/dromara/thingsbrain/persistence/jpa/logic/generator/HerodotusMqttAccountUuidGeneratorType.java
@@ -26,7 +26,7 @@
 package org.dromara.thingsbrain.persistence.jpa.logic.generator;
 
 import org.apache.commons.lang3.StringUtils;
-import org.dromara.dante.data.hibernate.generator.AbstractIdGeneratorType;
+import org.dromara.dante.hibernate.generator.AbstractIdGeneratorType;
 import org.dromara.thingsbrain.persistence.jpa.logic.entity.HerodotusMqttAccount;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.generator.GeneratorCreationContext;
