d872d8a9a792f714ec70f91dff72f8adc96e7dee
码匠君 <pointer_v@qq.com>
2026-05-04 22:16:02 +0800

    [重构] 重构 thingsbrain-link 相关模块代码

--- thingsbrain-link/thingsbrain-link-autoconfigure/src/main/java/org/dromara/thingsbrain/link/autoconfigure/initializer/MqttSubscribeTopicSender.java
+++ thingsbrain-link/thingsbrain-link-autoconfigure/src/main/java/org/dromara/thingsbrain/link/autoconfigure/initializer/MqttSubscribeTopicSender.java
@@ -1,22 +1,26 @@
 /*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * Copyright 2020-2030 码匠君<herodotus@aliyun.com>
  *
- * Copyright (c) 2020-2030 郑庚伟 ZHENGGENGWEI (码匠君), <herodotus@aliyun.com> Licensed under the AGPL License
+ * ThingsBrain licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This file is part of Herodotus ThingsBrain.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Herodotus ThingsBrain is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * Herodotus ThingsBrain is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * ThingsBrain 是 Dante Cloud 系统生态产品，采用 APACHE LICENSE 2.0 开源协议，您在使用过程中，需要注意以下几点：
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.herodotus.vip>.
+ * 1. 请不要删除和修改根目录下的LICENSE文件。
+ * 2. 请不要删除和修改 ThingsBrain 源码头部的版权声明。
+ * 3. 请保留源码和相关描述文件的项目出处，作者声明等。
+ * 4. 分发源码时候，请注明软件出处 <https://gitee.com/dromara/dante-cloud>
+ * 5. 在修改包名，模块名称，项目代码等时，请注明软件出处 <https://gitee.com/dromara/dante-cloud>
+ * 6. 若您的项目无法满足以上几点，可申请商业授权
  */
 
 package org.dromara.thingsbrain.link.autoconfigure.initializer;
@@ -33,16 +37,16 @@ import java.util.Optional;
 
 /**
  * <p>Description: 系统 Mqtt 订阅主题发送器 </p>
- *
+ * <p>
  * 在启动正常启动之后，获取到平台对应的必要的订阅主题，以 Event 的方式将订阅主题信息发送到 infrastructure-module-mqtt 模块中动态添加以实现主题的订阅
- *
+ * <p>
  * 之所以要使用这种方式的原因：
  * 1. 实现系统订阅主题的动态管理，如果采用配置文件的方式配置，无法动态进行修改和管理
  * 2. 从模块依赖关系角度考虑，infrastructure-module-mqtt 模块并没有依赖 assemble 相关模块，相反 infrastructure-core 被 assemble-core 依赖，导致无法调用和数据层相关的代码
  * 3. 从模块解耦的角度考虑，infrastructure-module-mqtt 相对独立，仅有一个获取订阅主题的特殊需求，如果直接依赖数据层相关的模块，徒增耦合性无法提升独立性。
- *
+ * <p>
  * 所以采取 Event 的方式，系统启动后，读取订阅数据，然后发送到 infrastructure-module-mqtt 模块中。
- *
+ * <p>
  * 目前，还不支持数据库表变化后，同步变化。后续看功能需要，适时添加。
  *
  * @author : gengwei.zheng
